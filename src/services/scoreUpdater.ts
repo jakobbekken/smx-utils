df20d8a1165c111caee37547d5796b88097703ed
Jakob Moen Bekken <jakobmb@uia.no>
2024-12-07 13:42:08 +0100

    Added embed on score updates
    Co-authored-by: Fisk0809 <hfstadtler@gmail.com>

--- src/services/scoreUpdater.ts
+++ src/services/scoreUpdater.ts
@@ -1,4 +1,4 @@
-import { Client, TextChannel } from "discord.js"
+import { Client, EmbedBuilder, TextChannel } from "discord.js"
 import { getAllScoreChannels } from "../models/scoreChannelModel";
 
 let lastTime: string = "";
@@ -68,12 +68,49 @@ export const startScoreUpdater = async (client: Client, interval: number) => {
 
               for (const score of data.reverse()) {
 
+                // Song info
                 const title = score.song.title;
+                const artist = score.song.artist;
+                const bpm = score.song.bpm;
+                const cover = `https://data.stepmaniax.com/${score.song.cover_thumb}`;
+                const genre = score.song.genre;
+
+                // Score Info
                 const difficultyDisplay = score.chart.difficulty_display;
                 const difficulty = score.chart.difficulty;
+                const gameScore = score.score;
+                const scoreId = score._id;
+                const thumbnail = `https://scores.stepmaniax.com/image/${scoreId}`;
+                const maxCombo = score.max_combo;
+                const calories = score.calories;
+
+                // SMX Info
                 const smxUsername = score.gamer.username;
+                const smxDescription = score.gamer.description;
+                const smxCountry = score.gamer.country;
+                const smxPicture = `https://data.stepmaniax.com/${score.gamer.picture_path}`;
+                const smxColor = score.gamer.hex_color;
+
+                const scoreEmbed = new EmbedBuilder()
+                  .setTitle(`${smxUsername} :flag_${smxCountry.toLowerCase()}:`)
+                  .setDescription(smxDescription)
+                  .setColor(`#${smxColor}`)
+                  .setURL(`https://smx.573.no/graph?scores=${scoreId}`)
+                  .setAuthor({ name: `${title} - ${artist}`, iconURL: cover })
+                  .setImage(thumbnail)
+                  .setThumbnail(smxPicture)
+                  .addFields(
+                    { name: "Score", value: gameScore.toLocaleString("en-US"), inline: true },
+                    { name: "Difficulty", value: `${difficultyDisplay.charAt(0).toUpperCase() + difficultyDisplay.slice(1)} ${difficulty}`, inline: true },
+                    { name: "Max Combo", value: `${maxCombo}`, inline: true },
+                    { name: "BPM", value: `${bpm}`, inline: true },
+                    { name: "Calories", value: `${calories}`, inline: true },
+                    { name: "Genre", value: `${genre.charAt(0).toUpperCase() + genre.slice(1)}`, inline: true },
+                  )
+                  .setTimestamp()
                 
-                await textChannel.send(`${smxUsername} played ${title} at ${difficultyDisplay} ${difficulty}`);
+                
+                await textChannel.send({ embeds: [scoreEmbed] });
               }
             }
           } catch (error) {
